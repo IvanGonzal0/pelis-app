a39f4f655d16e552668f587b45997408e1582819
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-10-03 12:38:35 -0300

    Component Header.jsx

---
+++ cine-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">Cine App</h1>
+      
+    </header>
+  );
+}
+
+export default Header;
